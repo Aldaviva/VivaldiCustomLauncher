de43c6810c7701ac1e5880d6d58b344227dd230d
Ben Hutchison <ben@aldaviva.com>
2019-07-08 10:12:51 -0700

    Change from image hijacking to relying on file type and URI scheme associations to intercept Vivaldi launches. Change code formatting style. Remove GUI.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # VivaldiCustomLauncher
 Intercept executions of Vivaldi to add custom arguments and apply tweaks files
 
@@ -8,19 +9,22 @@ This codebase is an absolute mess.
 1. Import it into Visual Studio Community 2017 or better.
 1. Build the solution.
 1. Copy `VivaldiCustomLauncher.exe` somewhere.
-1. Run `VivaldiCustomLauncher.exe`.
-1. Enable hijacking.
-1. Try running Vivaldi.
+1. Update your file type and URL scheme associations to use this program instead of `vivaldi.exe`, *e.g.*
+	-- `HKEY_CLASSES_ROOT\http\shell\open\command`
+    -- `HKEY_CLASSES_ROOT\https\shell\open\command`
+    -- `HKEY_CLASSES_ROOT\VivaldiHTM.*\shell\open\command`
+1. Update any shortcuts to `vivaldi.exe` to refer to this program instead, for example, shortcuts in the Start Menu.
+1. Try opening a web page.
 1. Tweaks and arguments will be applied.
 
 # What does it do
-- Always run Vivaldi with the  `--force-renderer-accessibility` argument so KeePass can read the URL bar and auto-type passwords. Otherwise, this is a nonpersistent setting that constantly breaks the password manager.
-- Include a [custom style sheet](https://gist.github.com/Aldaviva/9fbe321331b7f80786a371e0fd4bcfaf#file-style-custom-css) in the browser chrome to clean up the UI and make it more minimalistic.
-- Tweak the browser bundle script to [make tabs stretch to fill the full width of the tab bar](https://gist.github.com/Aldaviva/39e4472ab7a5ee50473de74df826d928).
+- Always runs Vivaldi with the  `--force-renderer-accessibility` argument so [KeePass](https://keepass.info/) can read the URL bar and auto-type passwords. Without this launcher, this is a nonpersistent setting that constantly breaks the password manager.
+- Includes a [custom style sheet](https://gist.github.com/Aldaviva/9fbe321331b7f80786a371e0fd4bcfaf#file-style-custom-css) in the browser chrome to clean up the UI and make it more minimal.
+- Include a [custom script](https://gist.github.com/Aldaviva/9fbe321331b7f80786a371e0fd4bcfaf#file-scripts-custom-js) to add more keyboard shortcuts to the browser.
+- Tweaks the browser bundle script to [make tabs stretch to fill the full width of the tab bar](https://gist.github.com/Aldaviva/39e4472ab7a5ee50473de74df826d928).
 - Automatically reapply all of the above tweaks when the browser is restarted or upgraded.
 
 # How does it work
-1. The hijack button sets an Image File Execution Option on Vivaldi. This way, when you try to start Vivaldi, Windows runs this program instead.
-1. This program checks to see if the tweaks need to be reapplied after an upgrade. If any of the tweaked files are out-of-date, they are automatically updated.
-1. This program makes a copy of the Vivaldi executable, which is an implementation requirement of using Image File Execution Options.
+1. When you open an HTML page, web URL, or a shortcut to Vivaldi, this headless launcher program is started instead, because you updated all the associations and shortcuts for Vivaldi.
+1. This program checks to see if the tweaks need to be applied, including after an upgrade. If any of the tweaked files are out-of-date, they are automatically updated.
 1. This program launches Vivaldi, passing in your original arguments, and adding the default arguments above.
--- VivaldiCustomLauncher/Program.cs
+++ VivaldiCustomLauncher/Program.cs
@@ -8,95 +8,56 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
-namespace VivaldiCustomLauncher
-{
-    public static class Program
-    {
+namespace VivaldiCustomLauncher {
+
+    public static class Program {
+
         private static HttpClient _client;
 
         [STAThread]
-        public static void Main()
-        {
-            Stopwatch stopwatch = Stopwatch.StartNew();
+        public static void Main() {
+            string processToRun = Path.Combine(GetVivaldiApplicationDirectory(), "vivaldi.exe");
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            string resourceDirectory = GetResourceDirectory(Path.GetDirectoryName(processToRun));
 
-            if (Environment.GetCommandLineArgs().Length == 1)
-            {
-                Application.Run(new ConfigurationDialog());
-            }
-            else
-            {
-                string processToRun = Environment.GetCommandLineArgs()[1];
+            ApplyTweaks(resourceDirectory);
 
-                string resourceDirectory = GetResourceDirectory(Path.GetDirectoryName(processToRun));
+            IEnumerable<string> originalArguments = Environment.GetCommandLineArgs().Skip(1);
+            string processArgumentsToRun = CommandLine.ArgvToCommandLine(CustomizeArguments(originalArguments));
 
-                ApplyTweaks(resourceDirectory);
-
-                //make non-debugged copy of executable so we don't get into an infinite loop of launching the real Vivaldi, only to have it start this launcher again
-                string processDirectory = Path.GetDirectoryName(processToRun) ??
-                                          throw new InvalidOperationException(
-                                              "Could not get directory name of path " + processToRun);
-                string renamedProcess = Path.Combine(processDirectory,
-                    Path.GetFileNameWithoutExtension(processToRun) + "2" + Path.GetExtension(processToRun));
-                if (IsBrowserExecutableDifferent(processToRun, renamedProcess))
-                {
-                    try
-                    {
-                        File.Copy(processToRun, renamedProcess, true);
-                    }
-                    catch (IOException)
-                    {
-                        //failed to copy browser to a non-debugged copy, so ignore.
-                        //can be caused by the browser already running when this program runs, so there is already a write lock on the executable
-                    }
-                }
-
-                IEnumerable<string> originalArguments = Environment.GetCommandLineArgs().Skip(2);
-                string processArgumentsToRun = CommandLine.ArgvToCommandLine(CustomizeArguments(originalArguments));
-
-                CreateProcess(renamedProcess, processArgumentsToRun);
-                stopwatch.Stop();
-//                MessageBox.Show($"Launched Vivaldi in {stopwatch.ElapsedMilliseconds} ms.", "VivaldiCustomLauncher", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-//                MessageBox.Show($"Started {renamedProcess} {processArgumentsToRun}\n\nPID: {pid}", "Success", MessageBoxButtons.OK,
-//                    MessageBoxIcon.Information);
-            }
+            CreateProcess(processToRun, processArgumentsToRun);
+//            MessageBox.Show($"Started {processToRun} {processArgumentsToRun}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private static void ApplyTweaks(string resourceDirectory)
-        {
+        private static void ApplyTweaks(string resourceDirectory) {
             string customStyleSheetRelativePath = Path.Combine("style", "custom.css");
-            string customStylSheetAbsolutePath = Path.Combine(resourceDirectory, customStyleSheetRelativePath);
+            string customStyleSheetAbsolutePath = Path.Combine(resourceDirectory, customStyleSheetRelativePath);
 
             string customScriptRelativePath = Path.Combine("scripts", "custom.js");
             string customScriptAbsolutePath = Path.Combine(resourceDirectory, customScriptRelativePath);
 
-            Task.WaitAll(
-                TweakBrowserHtml(Path.Combine(resourceDirectory, "browser.html"), customStyleSheetRelativePath,
-                    customScriptRelativePath),
-                TweakCustomStyleSheet(customStylSheetAbsolutePath),
-                TweakBundleScript(Path.Combine(resourceDirectory, "bundle.js")),
+            string bundleScriptAbsolutePath = Path.Combine(resourceDirectory, "bundle.js");
+            string browserPageAbsolutePath = Path.Combine(resourceDirectory, "browser.html");
+
+            Task.WaitAll(TweakBrowserHtml(browserPageAbsolutePath, customStyleSheetRelativePath, customScriptRelativePath),
+                TweakCustomStyleSheet(customStyleSheetAbsolutePath),
+                TweakBundleScript(bundleScriptAbsolutePath),
                 TweakCustomScript(customScriptAbsolutePath));
         }
 
-        private static async Task TweakBundleScript(string bundleScriptFile)
-        {
+        private static async Task TweakBundleScript(string bundleScriptFile) {
             const string customizedComment = @"/* Customized by Ben */";
             char[] expectedHeader = customizedComment.ToCharArray();
 
-            using (FileStream file = File.Open(bundleScriptFile, FileMode.Open, FileAccess.ReadWrite))
-            {
+            using (FileStream file = File.Open(bundleScriptFile, FileMode.Open, FileAccess.ReadWrite)) {
                 string bundleContents;
-                using (var reader = new StreamReader(file, Encoding.UTF8, false, 4 * 1024, true))
-                {
+                using (var reader = new StreamReader(file, Encoding.UTF8, false, 4 * 1024, true)) {
                     var buffer = new char[expectedHeader.Length];
                     await reader.ReadAsync(buffer, 0, buffer.Length);
 
-                    if (expectedHeader.SequenceEqual(buffer))
-                    {
+                    if (expectedHeader.SequenceEqual(buffer)) {
                         return;
                     }
 
@@ -105,17 +66,17 @@ namespace VivaldiCustomLauncher
                     bundleContents = await reader.ReadToEndAsync();
 
                     // Increase maximum tab width
-                    bundleContents = Regex.Replace(bundleContents, @"(?<prefix>TabStrip\.jsx.{1,1000}?=)(?<minTabWidth>180)(?<suffix>,)",
-                        match => match.Groups["prefix"].Value + 2000 + customizedComment + match.Groups["suffix"].Value);
+                    bundleContents = Regex.Replace(bundleContents,
+                        @"(?<prefix>TabStrip\.jsx.{1,1000}=)(?<minTabWidth>180)(?<suffix>,)",
+                        match => match.Groups["prefix"].Value + 4000 + customizedComment + match.Groups["suffix"].Value);
 
                     // Remove extra spacing on the right side of tab bar
                     bundleContents = Regex.Replace(bundleContents,
-                        @"(?<prefix>getStyles"",e=>.{1,200}?this\.props\.maxWidth)(?<suffix>,)",
-                        match => match.Groups["prefix"].Value + @"+62" + customizedComment + match.Groups["suffix"].Value);
+                        @"(?<prefix>getStyle.{1,20}e=>.{1,200}this\.props\.maxWidth)(?<suffix>,)",
+                        match => match.Groups["prefix"].Value + "+62" + customizedComment + match.Groups["suffix"].Value);
                 }
 
-                using (var writer = new StreamWriter(file, Encoding.UTF8))
-                {
+                using (var writer = new StreamWriter(file, Encoding.UTF8)) {
                     file.Seek(0, SeekOrigin.Begin);
                     await writer.WriteAsync(expectedHeader);
                     await writer.WriteAsync(bundleContents);
@@ -124,11 +85,8 @@ namespace VivaldiCustomLauncher
             }
         }
 
-
-        private static async Task TweakCustomScript(string customScriptFile)
-        {
-            if (!File.Exists(customScriptFile))
-            {
+        private static async Task TweakCustomScript(string customScriptFile) {
+            if (!File.Exists(customScriptFile)) {
                 Directory.CreateDirectory(Path.GetDirectoryName(customScriptFile) ??
                                           throw new InvalidOperationException(
                                               "Could not get path of custom script file " + customScriptFile));
@@ -136,24 +94,19 @@ namespace VivaldiCustomLauncher
                 using (FileStream fileStream = File.OpenWrite(customScriptFile))
                 using (Stream downloadStream = await GetClient().GetStreamAsync(
                     @"https://gist.githubusercontent.com/Aldaviva/9fbe321331b7f80786a371e0fd4bcfaf/raw/%257C%2520scripts%255Ccustom.js")
-                )
-                {
+                ) {
                     await downloadStream.CopyToAsync(fileStream);
                     await fileStream.FlushAsync();
                 }
             }
         }
 
-
-        private static async Task TweakCustomStyleSheet(string customStylSheetFile)
-        {
-            if (!File.Exists(customStylSheetFile))
-            {
-                using (FileStream fileStream = File.OpenWrite(customStylSheetFile))
+        private static async Task TweakCustomStyleSheet(string customStyleSheetFile) {
+            if (!File.Exists(customStyleSheetFile)) {
+                using (FileStream fileStream = File.OpenWrite(customStyleSheetFile))
                 using (Stream downloadStream = await GetClient().GetStreamAsync(
                     @"https://gist.githubusercontent.com/Aldaviva/9fbe321331b7f80786a371e0fd4bcfaf/raw/%257C%2520style%255Ccustom.css")
-                )
-                {
+                ) {
                     await downloadStream.CopyToAsync(fileStream);
                     await fileStream.FlushAsync();
                 }
@@ -161,8 +114,7 @@ namespace VivaldiCustomLauncher
         }
 
         private static async Task TweakBrowserHtml(string browserHtmlFile, string customStyleSheetRelativeFilePath,
-            string customScriptRelativePath)
-        {
+            string customScriptRelativePath) {
             string fileContents = File.ReadAllText(browserHtmlFile, Encoding.UTF8);
             string styleSheetRelativeUri = new UriBuilder { Path = customStyleSheetRelativeFilePath }.Path;
             string scriptRelativeUri = new UriBuilder { Path = customScriptRelativePath }.Path;
@@ -170,38 +122,42 @@ namespace VivaldiCustomLauncher
             bool fileModified = false;
             string modifiedFileContents = fileContents;
 
-            if (!fileContents.Contains(styleSheetRelativeUri))
-            {
+            if (!fileContents.Contains(styleSheetRelativeUri)) {
                 modifiedFileContents = modifiedFileContents
                     .Replace(@"  </head>", $"    <link rel=\"stylesheet\" href=\"{styleSheetRelativeUri}\" />\n  </head>");
                 fileModified = true;
             }
 
-            if (!fileContents.Contains(scriptRelativeUri))
-            {
+            if (!fileContents.Contains(scriptRelativeUri)) {
                 modifiedFileContents = modifiedFileContents.Replace(@"  </body>",
                     $"    <script src=\"{scriptRelativeUri}\"></script>\n  </body>");
                 fileModified = true;
             }
 
-            if (fileModified)
-            {
+            if (fileModified) {
                 using (FileStream writeStream = File.OpenWrite(browserHtmlFile))
-                {
-                    using (var writer = new StreamWriter(writeStream, Encoding.UTF8))
-                    {
-                        await writer.WriteAsync(modifiedFileContents);
-                        await writer.FlushAsync();
-                    }
+                using (var writer = new StreamWriter(writeStream, Encoding.UTF8)) {
+                    await writer.WriteAsync(modifiedFileContents);
+                    await writer.FlushAsync();
                 }
             }
         }
 
-        private static string GetResourceDirectory(string applicationDirectory)
-        {
+        private static string GetVivaldiApplicationDirectory() {
+            using (RegistryKey uninstallKey =
+                Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall\Vivaldi", false)) {
+                if (uninstallKey == null) {
+                    throw new InvalidOperationException("Could not find Vivaldi uninstallation key in user registry hive" +
+                                                        @" (HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\Vivaldi)");
+                }
+
+                return (string) uninstallKey.GetValue("InstallLocation");
+            }
+        }
+
+        private static string GetResourceDirectory(string applicationDirectory) {
             string versionDirectory = Directory.EnumerateDirectories(applicationDirectory)
-                .Last(absoluteSubdirectory =>
-                {
+                .Last(absoluteSubdirectory => {
                     string relativeSubdirectory = Path.GetFileName(absoluteSubdirectory) ??
                                                   throw new InvalidOperationException(
                                                       "No final directory component in path " + absoluteSubdirectory);
@@ -211,37 +167,20 @@ namespace VivaldiCustomLauncher
             return Path.Combine(versionDirectory, "resources", "vivaldi");
         }
 
-        private static bool IsBrowserExecutableDifferent(string processToRun, string renamedProcess)
-        {
-            var originInfo = new FileInfo(processToRun);
-            var destinationInfo = new FileInfo(renamedProcess);
-            if (destinationInfo.Exists)
-            {
-                bool isDateModifiedDifferent = originInfo.LastWriteTimeUtc != destinationInfo.LastWriteTimeUtc;
-                bool isSizeDifferent = originInfo.Length != destinationInfo.Length;
-                return isDateModifiedDifferent || isSizeDifferent;
-            }
-            else
-            {
-                return true;
-            }
-        }
-
-        private static IEnumerable<string> CustomizeArguments(IEnumerable<string> originalArguments)
-        {
+        private static IEnumerable<string> CustomizeArguments(IEnumerable<string> originalArguments) {
             IList<string> customizedArguments = originalArguments.ToList();
             customizedArguments.Insert(0, "--force-renderer-accessibility");
             return customizedArguments;
         }
 
-        private static int CreateProcess(string process, string arguments)
-        {
+        private static int CreateProcess(string process, string arguments) {
             return Process.Start(process, arguments)?.Id ?? -1;
         }
 
-        private static HttpClient GetClient()
-        {
+        private static HttpClient GetClient() {
             return _client ?? (_client = new HttpClient());
         }
+
     }
+
 }
