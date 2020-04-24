39f2c52a562f99878bf9c07eb25d2a56450e5b0d
Ben Hutchison <1417794+Aldaviva@users.noreply.github.com>
2020-04-24 03:05:21 -0700

    Update tab bar maximum width for Vivaldi 3.0 changes.
    Update program runtime to .NET Framework 4.8.

--- VivaldiCustomLauncher/Program.cs
+++ VivaldiCustomLauncher/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -14,39 +16,45 @@ namespace VivaldiCustomLauncher {
 
     public static class Program {
 
-        private static HttpClient _client;
+        private static HttpClient? HTTP_CLIENT;
+        private static HttpClient httpClient => HTTP_CLIENT ??= new HttpClient();
 
         [STAThread]
         public static void Main() {
+            var stopwatch = Stopwatch.StartNew();
+
             Application.EnableVisualStyles();
 
-            Application.ThreadException += (sender, args) => OnUncaughtException(args.Exception);
-            AppDomain.CurrentDomain.UnhandledException += (sender, args) => OnUncaughtException((Exception) args.ExceptionObject);
+            Application.ThreadException += (sender, args) => onUncaughtException(args.Exception);
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) => onUncaughtException((Exception) args.ExceptionObject);
 
             try {
-                string processToRun = Path.Combine(GetVivaldiApplicationDirectory(), "vivaldi.exe");
+                string processToRun = Path.Combine(getVivaldiApplicationDirectory(), "vivaldi.exe");
 
-                string resourceDirectory = GetResourceDirectory(Path.GetDirectoryName(processToRun));
+                string resourceDirectory = getResourceDirectory(Path.GetDirectoryName(processToRun));
 
-                ApplyTweaks(resourceDirectory);
+                applyTweaks(resourceDirectory);
 
                 IEnumerable<string> originalArguments = Environment.GetCommandLineArgs().Skip(1);
-                string processArgumentsToRun = CommandLine.ArgvToCommandLine(CustomizeArguments(originalArguments));
+                string processArgumentsToRun = CommandLine.ArgvToCommandLine(customizeArguments(originalArguments));
 
-                CreateProcess(processToRun, processArgumentsToRun);
-//            MessageBox.Show($"Started {processToRun} {processArgumentsToRun}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            } catch (Exception e) {
-                OnUncaughtException(e);
+                createProcess(processToRun, processArgumentsToRun);
+                stopwatch.Stop();
+
+//                MessageBox.Show($"Started {processToRun} {processArgumentsToRun} in {stopwatch.ElapsedMilliseconds:N0} ms", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } catch (Exception e) when(!(e is OutOfMemoryException)) {
+                onUncaughtException(e);
                 throw;
             }
         }
 
-        private static void OnUncaughtException(Exception e) {
+        private static void onUncaughtException(Exception e) {
             string message = (e as AggregateException)?.InnerException?.Message ?? e.Message;
-            MessageBox.Show($"{e.GetType().Name}: {message}", "Failed to tweak and launch Vivaldi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            MessageBox.Show($"{e.GetType().Name}: {message}", "Failed to tweak and launch Vivaldi", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
         }
 
-        private static void ApplyTweaks(string resourceDirectory) {
+        private static void applyTweaks(string resourceDirectory) {
             string customStyleSheetRelativePath = Path.Combine("style", "custom.css");
             string customStyleSheetAbsolutePath = Path.Combine(resourceDirectory, customStyleSheetRelativePath);
 
@@ -56,79 +64,73 @@ namespace VivaldiCustomLauncher {
             string bundleScriptAbsolutePath = Path.Combine(resourceDirectory, "bundle.js");
             string browserPageAbsolutePath = Path.Combine(resourceDirectory, "browser.html");
 
-            Task.WaitAll(TweakBrowserHtml(browserPageAbsolutePath, customStyleSheetRelativePath, customScriptRelativePath),
-                TweakCustomStyleSheet(customStyleSheetAbsolutePath),
-                TweakBundleScript(bundleScriptAbsolutePath),
-                TweakCustomScript(customScriptAbsolutePath));
+            Task.WaitAll(tweakBrowserHtml(browserPageAbsolutePath, customStyleSheetRelativePath, customScriptRelativePath),
+                tweakCustomStyleSheet(customStyleSheetAbsolutePath),
+                tweakBundleScript(bundleScriptAbsolutePath),
+                tweakCustomScript(customScriptAbsolutePath));
         }
 
-        private static async Task TweakBundleScript(string bundleScriptFile) {
-            const string customizedComment = @"/* Customized by Ben */";
-            char[] expectedHeader = customizedComment.ToCharArray();
+        private static async Task tweakBundleScript(string bundleScriptFile) {
+            const string CUSTOMIZED_COMMENT = @"/* Customized by Ben */";
+            char[] expectedHeader = CUSTOMIZED_COMMENT.ToCharArray();
 
-            using (FileStream file = File.Open(bundleScriptFile, FileMode.Open, FileAccess.ReadWrite)) {
-                string bundleContents;
-                using (var reader = new StreamReader(file, Encoding.UTF8, false, 4 * 1024, true)) {
-                    var buffer = new char[expectedHeader.Length];
-                    await reader.ReadAsync(buffer, 0, buffer.Length);
+            using FileStream file = File.Open(bundleScriptFile, FileMode.Open, FileAccess.ReadWrite);
+            string bundleContents;
+            using (var reader = new StreamReader(file, Encoding.UTF8, false, 4 * 1024, true)) {
+                var buffer = new char[expectedHeader.Length];
+                await reader.ReadAsync(buffer, 0, buffer.Length);
 
-                    if (expectedHeader.SequenceEqual(buffer)) {
-                        return;
-                    }
-
-                    file.Seek(0, SeekOrigin.Begin);
-                    reader.DiscardBufferedData();
-                    bundleContents = await reader.ReadToEndAsync();
-
-                    // Increase maximum tab width
-                    bundleContents = Regex.Replace(bundleContents,
-                        @"(?<prefix>TabStrip\.jsx.{1,1000}=)(?<minTabWidth>180)(?<suffix>,)",
-                        match => match.Groups["prefix"].Value + 4000 + customizedComment + match.Groups["suffix"].Value);
-
-                    // Remove extra spacing on the right side of tab bar
-                    bundleContents = Regex.Replace(bundleContents,
-                        @"(?<prefix>getStyle.{1,20}e=>.{1,200}this\.props\.maxWidth)(?<suffix>,)",
-                        match => match.Groups["prefix"].Value + "+62" + customizedComment + match.Groups["suffix"].Value);
+                if (expectedHeader.SequenceEqual(buffer)) {
+                    return;
                 }
 
-                using (var writer = new StreamWriter(file, Encoding.UTF8)) {
-                    file.Seek(0, SeekOrigin.Begin);
-                    await writer.WriteAsync(expectedHeader);
-                    await writer.WriteAsync(bundleContents);
-                    await writer.FlushAsync();
-                }
+                file.Seek(0, SeekOrigin.Begin);
+                reader.DiscardBufferedData();
+                bundleContents = await reader.ReadToEndAsync();
+
+                // Increase maximum tab width
+                bundleContents = Regex.Replace(bundleContents,
+                    @"(?<prefix>\bmaxWidth=)(?<minTabWidth>180)(?<suffix>,)",
+                    match => match.Groups["prefix"].Value + 4000 + CUSTOMIZED_COMMENT + match.Groups["suffix"].Value);
+
+                // Remove extra spacing on the right side of tab bar
+                bundleContents = Regex.Replace(bundleContents,
+                    @"(?<prefix>\bgetStyle.{1,20}e=>.{1,200}this\.props\.maxWidth)(?<suffix>,)",
+                    match => match.Groups["prefix"].Value + "+62" + CUSTOMIZED_COMMENT + match.Groups["suffix"].Value);
             }
+
+            using var writer = new StreamWriter(file, Encoding.UTF8);
+            file.Seek(0, SeekOrigin.Begin);
+            await writer.WriteAsync(expectedHeader);
+            await writer.WriteAsync(bundleContents);
+            await writer.FlushAsync();
         }
 
-        private static async Task TweakCustomScript(string customScriptFile) {
+        private static async Task tweakCustomScript(string customScriptFile) {
             if (!File.Exists(customScriptFile)) {
                 Directory.CreateDirectory(Path.GetDirectoryName(customScriptFile) ??
                                           throw new InvalidOperationException(
                                               "Could not get path of custom script file " + customScriptFile));
 
-                using (FileStream fileStream = File.OpenWrite(customScriptFile))
-                using (Stream downloadStream = await GetClient().GetStreamAsync(
-                    @"https://gist.githubusercontent.com/Aldaviva/9fbe321331b7f80786a371e0fd4bcfaf/raw/%257C%2520scripts%255Ccustom.js")
-                ) {
-                    await downloadStream.CopyToAsync(fileStream);
-                    await fileStream.FlushAsync();
-                }
+                using FileStream fileStream = File.OpenWrite(customScriptFile);
+                using Stream downloadStream = await httpClient.GetStreamAsync(
+                    @"https://gist.githubusercontent.com/Aldaviva/9fbe321331b7f80786a371e0fd4bcfaf/raw/%257C%2520scripts%255Ccustom.js");
+                await downloadStream.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
             }
         }
 
-        private static async Task TweakCustomStyleSheet(string customStyleSheetFile) {
+        private static async Task tweakCustomStyleSheet(string customStyleSheetFile) {
             if (!File.Exists(customStyleSheetFile)) {
-                using (FileStream fileStream = File.OpenWrite(customStyleSheetFile))
-                using (Stream downloadStream = await GetClient().GetStreamAsync(
-                    @"https://gist.githubusercontent.com/Aldaviva/9fbe321331b7f80786a371e0fd4bcfaf/raw/%257C%2520style%255Ccustom.css")
-                ) {
-                    await downloadStream.CopyToAsync(fileStream);
-                    await fileStream.FlushAsync();
-                }
+                using FileStream fileStream = File.OpenWrite(customStyleSheetFile);
+                using Stream downloadStream = await httpClient.GetStreamAsync(
+                    @"https://gist.githubusercontent.com/Aldaviva/9fbe321331b7f80786a371e0fd4bcfaf/raw/%257C%2520style%255Ccustom.css");
+                await downloadStream.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
             }
         }
 
-        private static async Task TweakBrowserHtml(string browserHtmlFile, string customStyleSheetRelativeFilePath,
+        private static async Task tweakBrowserHtml(string browserHtmlFile, string customStyleSheetRelativeFilePath,
             string customScriptRelativePath) {
             string fileContents = File.ReadAllText(browserHtmlFile, Encoding.UTF8);
             string styleSheetRelativeUri = new UriBuilder { Path = customStyleSheetRelativeFilePath }.Path;
@@ -150,15 +152,14 @@ namespace VivaldiCustomLauncher {
             }
 
             if (fileModified) {
-                using (FileStream writeStream = File.OpenWrite(browserHtmlFile))
-                using (var writer = new StreamWriter(writeStream, Encoding.UTF8)) {
-                    await writer.WriteAsync(modifiedFileContents);
-                    await writer.FlushAsync();
-                }
+                using FileStream writeStream = File.OpenWrite(browserHtmlFile);
+                using var writer = new StreamWriter(writeStream, Encoding.UTF8);
+                await writer.WriteAsync(modifiedFileContents);
+                await writer.FlushAsync();
             }
         }
 
-        private static string GetVivaldiApplicationDirectory() {
+        private static string getVivaldiApplicationDirectory() {
             IEnumerable<(RegistryKey, string)> uninstallKeys = new[] {
                 (Registry.CurrentUser, @"Software\Microsoft\Windows\CurrentVersion\Uninstall\Vivaldi"),
                 (Registry.CurrentUser, @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Vivaldi"),
@@ -167,40 +168,35 @@ namespace VivaldiCustomLauncher {
             };
 
             foreach ((RegistryKey hive, string path) in uninstallKeys) {
-                using (RegistryKey key = hive.OpenSubKey(path, false)) {
-                    if (key != null) {
-                        return (string) key.GetValue("InstallLocation");
-                    }
+                using RegistryKey key = hive.OpenSubKey(path, false);
+                if (key != null) {
+                    return (string) key.GetValue("InstallLocation");
                 }
             }
 
             throw new InvalidOperationException("Could not find Vivaldi uninstallation key in registry");
         }
 
-        private static string GetResourceDirectory(string applicationDirectory) {
+        private static string getResourceDirectory(string applicationDirectory) {
             string versionDirectory = Directory.EnumerateDirectories(applicationDirectory)
                 .Last(absoluteSubdirectory => {
                     string relativeSubdirectory = Path.GetFileName(absoluteSubdirectory) ??
                                                   throw new InvalidOperationException(
-                                                      "No final directory component in path " + absoluteSubdirectory);
+                                                      "No final directory component in path " + applicationDirectory);
                     return Regex.IsMatch(relativeSubdirectory, @"\A\d+\.\d+\.\d+\.\d+\z");
                 });
 
             return Path.Combine(versionDirectory, "resources", "vivaldi");
         }
 
-        private static IEnumerable<string> CustomizeArguments(IEnumerable<string> originalArguments) {
+        private static IEnumerable<string> customizeArguments(IEnumerable<string> originalArguments) {
             IList<string> customizedArguments = originalArguments.ToList();
             customizedArguments.Insert(0, "--force-renderer-accessibility");
             return customizedArguments;
         }
 
-        private static int CreateProcess(string process, string arguments) {
+        private static int createProcess(string process, string arguments) {
             return Process.Start(process, arguments)?.Id ?? -1;
-        }
-
-        private static HttpClient GetClient() {
-            return _client ?? (_client = new HttpClient());
         }
 
     }
