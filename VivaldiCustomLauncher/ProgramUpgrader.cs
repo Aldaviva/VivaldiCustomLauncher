a125137d7ca0e18d6cd5eaf16c78e47d1ccd867b
Ben Hutchison <ben@aldaviva.com>
2022-04-15 01:56:56 -0700

    #2: Automatically upgrade to the latest release.

---
+++ VivaldiCustomLauncher/ProgramUpgrader.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Utf8Json;
+
+#nullable enable
+
+namespace VivaldiCustomLauncher;
+
+internal class ProgramUpgrader {
+
+    private static readonly Uri RELEASE_URI = new("https://api.github.com/repos/Aldaviva/VivaldiCustomLauncher/releases/latest");
+
+    private readonly HttpClient            httpClient;
+    private readonly VersionNumberComparer versionNumberComparer;
+
+    public ProgramUpgrader(HttpClient httpClient, VersionNumberComparer versionNumberComparer) {
+        this.httpClient            = httpClient;
+        this.versionNumberComparer = versionNumberComparer;
+    }
+
+    /// <summary>
+    /// Install the latest version of this program from GitHub.
+    /// </summary>
+    /// <returns><c>true</c> if an upgrade is pending and the program should exit quickly, or <c>false</c> if no upgrade is pending and the program should resume execution</returns>
+    public async Task<bool> upgrade() {
+        return await getUpgradeUri() is { } upgradeUri && await installUpgrade(upgradeUri);
+    }
+
+    private async Task<Uri?> getUpgradeUri() {
+        using HttpResponseMessage metadataResponse = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, RELEASE_URI) {
+            Headers = { Accept = { new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json") } }
+        }, HttpCompletionOption.ResponseHeadersRead);
+
+        if (!metadataResponse.IsSuccessStatusCode) {
+            return null;
+        }
+
+        using Stream metadataStream = await metadataResponse.Content.ReadAsStreamAsync();
+        dynamic      metadata       = await JsonSerializer.DeserializeAsync<dynamic>(metadataStream);
+
+        string? latestVersion = metadata["tag_name"];
+        if (latestVersion is null) {
+            return null;
+        }
+
+        string currentVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString(3);
+
+        if (versionNumberComparer.Compare(latestVersion, currentVersion) > 0) {
+            return new Uri(metadata["assets"][0]["url"]);
+        } else {
+            return null;
+        }
+    }
+
+    private async Task<bool> installUpgrade(Uri upgradeUri) {
+        using HttpResponseMessage executableResponse = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, upgradeUri) {
+            Headers = { Accept = { new MediaTypeWithQualityHeaderValue("application/octet-stream") } }
+        }, HttpCompletionOption.ResponseHeadersRead);
+
+        if (!executableResponse.IsSuccessStatusCode) {
+            return false;
+        }
+
+        string executableAbsolutePath = Assembly.GetExecutingAssembly().Location;
+        string tempFile = Path.Combine(Path.GetDirectoryName(executableAbsolutePath)!,
+            Path.GetFileNameWithoutExtension(executableAbsolutePath) + "-" + generateRandomString(8) + Path.GetExtension(executableAbsolutePath) + ".tmp");
+
+        Stream fileStream;
+        try {
+            fileStream = new FileStream(tempFile, FileMode.Create, FileAccess.Write, FileShare.None, 4096, true);
+        } catch (Exception e) when (e is not OutOfMemoryException) {
+            return false;
+        }
+
+        using (fileStream) {
+            await executableResponse.Content.CopyToAsync(fileStream);
+            await fileStream.FlushAsync();
+        }
+
+        using Process? replacerProcess = Process.Start(new ProcessStartInfo {
+            FileName = "cmd.exe",
+            Arguments = "/q /c timeout /t 2 && " +
+                $"move /y \"{tempFile}\" \"{executableAbsolutePath}\" && " +
+                $"start \"\" {Environment.CommandLine}",
+            CreateNoWindow  = true,
+            UseShellExecute = false
+        });
+
+        return replacerProcess is not null;
+    }
+
+    /// https://stackoverflow.com/a/1344255/979493
+    private static string generateRandomString(int length) {
+        using RNGCryptoServiceProvider random = new();
+
+        char[] possibleChars       = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
+        int    possibleCharsLength = possibleChars.Length;
+
+        char[] result       = new char[length];
+        byte[] randomBuffer = new byte[length * 4];
+        random.GetBytes(randomBuffer);
+
+        while (length-- > 0) {
+            result[length] = possibleChars[BitConverter.ToUInt32(randomBuffer, length * 4) % possibleCharsLength];
+        }
+
+        return new string(result);
+    }
+
+}
--- VivaldiCustomLauncher/VivaldiLauncher.cs
+++ VivaldiCustomLauncher/VivaldiLauncher.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -13,154 +14,170 @@ using System.Windows.Forms;
 using Microsoft.Win32;
 using VivaldiCustomLauncher.Tweaks;
 
-namespace VivaldiCustomLauncher {
+namespace VivaldiCustomLauncher;
 
-    public static class VivaldiLauncher {
+public static class VivaldiLauncher {
 
-        private static HttpClient? HTTP_CLIENT;
-        private static HttpClient httpClient => HTTP_CLIENT ??= new HttpClient();
+    private static readonly VersionNumberComparer VERSION_NUMBER_COMPARER = new();
+    private static          HttpClient?           HTTP_CLIENT;
+    private static readonly AssemblyName          ASSEMBLY_NAME = Assembly.GetExecutingAssembly().GetName();
 
-        [STAThread]
-        public static int Main() {
-            Application.EnableVisualStyles();
+    private static HttpClient httpClient => HTTP_CLIENT ??= new HttpClient(new HttpClientHandler {
+        MaxConnectionsPerServer = 24,
+        AllowAutoRedirect       = true
+    }) {
+        Timeout               = TimeSpan.FromSeconds(3),
+        DefaultRequestHeaders = { UserAgent = { new ProductInfoHeaderValue(ASSEMBLY_NAME.Name, ASSEMBLY_NAME.Version.ToString()) } }
+    };
 
-            Application.ThreadException                += (_, args) => onUncaughtException(args.Exception);
-            AppDomain.CurrentDomain.UnhandledException += (_, args) => onUncaughtException((Exception) args.ExceptionObject);
+    [STAThread]
+    public static async Task<int> Main() {
+        Application.EnableVisualStyles();
 
-            return tweakAndLaunch() ? 0 : 1;
+        Application.ThreadException                += (_, args) => onUncaughtException(args.Exception);
+        AppDomain.CurrentDomain.UnhandledException += (_, args) => onUncaughtException((Exception) args.ExceptionObject);
+
+        try {
+            return await tweakAndLaunch() ? 0 : 1;
+        } finally {
+            HTTP_CLIENT?.Dispose();
         }
+    }
 
-        private static bool tweakAndLaunch() {
-            Stopwatch stopwatch = Stopwatch.StartNew();
-            bool      success   = true;
+    private static async Task<bool> tweakAndLaunch() {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        bool      success   = true;
+
+        try {
+            string processToRun = Path.Combine(getVivaldiApplicationDirectory(), "vivaldi.exe");
 
             try {
-                string processToRun = Path.Combine(getVivaldiApplicationDirectory(), "vivaldi.exe");
-
-                try {
-                    using Process? existingVivaldiProcess = Process.GetProcessesByName("vivaldi").FirstOrDefault();
-                    if (existingVivaldiProcess == null) {
-                        string resourceDirectory = getResourceDirectory(Path.GetDirectoryName(processToRun)!);
-                        applyTweaks(resourceDirectory);
+                using Process? existingVivaldiProcess = Process.GetProcessesByName("vivaldi").FirstOrDefault();
+                if (existingVivaldiProcess == null) {
+                    bool isInstallationPending = await new ProgramUpgrader(httpClient, VERSION_NUMBER_COMPARER).upgrade();
+                    if (isInstallationPending) {
+                        return true;
                     }
 
-                } catch (TweakException e) {
-                    MessageBox.Show($"Failed to apply tweak {e.tweakTypeName}.{e.tweakMethodName}: {e.bareMessage}", "Failed to tweak Vivaldi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    success = false;
+                    string resourceDirectory = getResourceDirectory(Path.GetDirectoryName(processToRun)!);
+                    await applyTweaks(resourceDirectory);
                 }
 
-                IEnumerable<string> originalArguments     = Environment.GetCommandLineArgs().Skip(1);
-                string              processArgumentsToRun = CommandLine.ArgvToCommandLine(customizeArguments(originalArguments));
-
-                createProcess(processToRun, processArgumentsToRun);
-                stopwatch.Stop();
-                // MessageBox.Show($"Started {processToRun} {processArgumentsToRun} in {stopwatch.ElapsedMilliseconds:N0} ms", "VivaldiCustomLauncher", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-            } catch (InvalidOperationException e) {
-                MessageBox.Show(e.Message, "Failed to launch Vivaldi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                success = false;
-            } catch (Exception e) when (e is not OutOfMemoryException) {
-                onUncaughtException(e);
+            } catch (TweakException e) {
+                MessageBox.Show($"Failed to apply tweak {e.tweakTypeName}.{e.tweakMethodName}: {e.bareMessage}", "Failed to tweak Vivaldi", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 success = false;
             }
 
-            return success;
+            IEnumerable<string> originalArguments     = Environment.GetCommandLineArgs().Skip(1);
+            string              processArgumentsToRun = CommandLine.ArgvToCommandLine(customizeArguments(originalArguments));
+
+            createProcess(processToRun, processArgumentsToRun);
+            stopwatch.Stop();
+            // MessageBox.Show($"Started {processToRun} {processArgumentsToRun} in {stopwatch.ElapsedMilliseconds:N0} ms", "VivaldiCustomLauncher", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        } catch (InvalidOperationException e) {
+            MessageBox.Show(e.Message, "Failed to launch Vivaldi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            success = false;
+        } catch (Exception e) when (e is not OutOfMemoryException) {
+            onUncaughtException(e);
+            success = false;
         }
 
-        private static void onUncaughtException(Exception e) {
-            string message = (e as AggregateException)?.InnerException?.Message ?? e.Message;
-            MessageBox.Show($"{e.GetType().Name}: {message}", "Failed to tweak and launch Vivaldi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return success;
+    }
+
+    private static void onUncaughtException(Exception e) {
+        string message = (e as AggregateException)?.InnerException?.Message ?? e.Message;
+        MessageBox.Show($"{e.GetType().Name}: {message}", "Failed to tweak and launch Vivaldi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    }
+
+    /// <exception cref="TweakException"></exception>
+    /// <exception cref="Exception">Ignore.</exception>
+    private static Task applyTweaks(string resourceDirectory) {
+        string customStyleSheetRelativePath = Path.Combine("style", "custom.css");
+        string customStyleSheetAbsolutePath = Path.Combine(resourceDirectory, customStyleSheetRelativePath);
+
+        string customScriptRelativePath = Path.Combine("scripts", "custom.js");
+        string customScriptAbsolutePath = Path.Combine(resourceDirectory, customScriptRelativePath);
+
+        string bundleScriptAbsolutePath                 = Path.Combine(resourceDirectory, "bundle.js");
+        string backgroundBundleCommonScriptAbsolutePath = Path.Combine(resourceDirectory, "background-common-bundle.js");
+        string browserPageAbsolutePath                  = Path.Combine(resourceDirectory, "browser.html");
+
+        string visualElementsSourcePath      = Path.Combine(resourceDirectory, "../../..", "vivaldi.VisualElementsManifest.xml");
+        string visualElementsDestinationPath = Path.Combine(resourceDirectory, "../../../..", ASSEMBLY_NAME.Name + ".VisualElementsManifest.xml");
+
+        try {
+            return Task.WhenAll(
+                applyTweakIfNecessary(new BrowserHtmlTweak(), new BrowserHtmlTweakParams(browserPageAbsolutePath, customStyleSheetRelativePath, customScriptRelativePath)),
+                applyTweakIfNecessary(new CustomStyleSheetTweak(httpClient), new BaseTweakParams(customStyleSheetAbsolutePath)),
+                applyTweakIfNecessary(new BundleScriptTweak(), new BaseTweakParams(bundleScriptAbsolutePath)),
+                applyTweakIfNecessary(new BackgroundCommonBundleScriptTweak(), new BaseTweakParams(backgroundBundleCommonScriptAbsolutePath)),
+                applyTweakIfNecessary(new CustomScriptTweak(httpClient), new BaseTweakParams(customScriptAbsolutePath)),
+                applyTweakIfNecessary(new VisualElementsManifestTweak(), new VisualElementsManifestTweakParams(visualElementsSourcePath, visualElementsDestinationPath)));
+        } catch (AggregateException e) {
+            if (e.InnerExceptions.Where(exception => exception is TweakException).Cast<TweakException>().FirstOrDefault() is { } tweakException) {
+                throw tweakException;
+            } else {
+                throw e.InnerException!;
+            }
+        }
+    }
+
+    /// <exception cref="TweakException"></exception>
+    private static async Task applyTweakIfNecessary<OutputType, Params>(Tweak<OutputType, Params> tweak, Params tweakParams) where Params: TweakParams where OutputType: class {
+        OutputType? editedFile = await tweak.readFileAndEditIfNecessary(tweakParams);
+        if (editedFile != null) {
+            await tweak.saveFile(editedFile, tweakParams);
+        }
+    }
+
+    /// <exception cref="InvalidOperationException"></exception>
+    private static string getVivaldiApplicationDirectory() {
+        if (Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\vivaldi.exe", "Path", null) is string appPath) {
+            return appPath;
         }
 
-        /// <exception cref="TweakException"></exception>
-        /// <exception cref="Exception">Ignore.</exception>
-        private static void applyTweaks(string resourceDirectory) {
-            string customStyleSheetRelativePath = Path.Combine("style", "custom.css");
-            string customStyleSheetAbsolutePath = Path.Combine(resourceDirectory, customStyleSheetRelativePath);
+        const string UNINSTALL_REGISTRY_PATH       = @"Software\Microsoft\Windows\CurrentVersion\Uninstall\Vivaldi";
+        const string UNINSTALL_REGISTRY_PATH_WOW64 = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Vivaldi";
+        IEnumerable<(RegistryKey hive, string path)> uninstallKeys = new[] {
+            (Registry.CurrentUser, UNINSTALL_REGISTRY_PATH),
+            (Registry.CurrentUser, UNINSTALL_REGISTRY_PATH_WOW64),
+            (Registry.LocalMachine, UNINSTALL_REGISTRY_PATH),
+            (Registry.LocalMachine, UNINSTALL_REGISTRY_PATH_WOW64)
+        };
 
-            string customScriptRelativePath = Path.Combine("scripts", "custom.js");
-            string customScriptAbsolutePath = Path.Combine(resourceDirectory, customScriptRelativePath);
-
-            string bundleScriptAbsolutePath                 = Path.Combine(resourceDirectory, "bundle.js");
-            string backgroundBundleCommonScriptAbsolutePath = Path.Combine(resourceDirectory, "background-common-bundle.js");
-            string browserPageAbsolutePath                  = Path.Combine(resourceDirectory, "browser.html");
-
-            string visualElementsSourcePath      = Path.Combine(resourceDirectory, "../../..", "vivaldi.VisualElementsManifest.xml");
-            string visualElementsDestinationPath = Path.Combine(resourceDirectory, "../../../..", Assembly.GetExecutingAssembly().GetName().Name + ".VisualElementsManifest.xml");
-
-            try {
-                Task.WaitAll(
-                    applyTweakIfNecessary(new BrowserHtmlTweak(), new BrowserHtmlTweakParams(browserPageAbsolutePath, customStyleSheetRelativePath, customScriptRelativePath)),
-                    applyTweakIfNecessary(new CustomStyleSheetTweak(httpClient), new BaseTweakParams(customStyleSheetAbsolutePath)),
-                    applyTweakIfNecessary(new BundleScriptTweak(), new BaseTweakParams(bundleScriptAbsolutePath)),
-                    applyTweakIfNecessary(new BackgroundCommonBundleScriptTweak(), new BaseTweakParams(backgroundBundleCommonScriptAbsolutePath)),
-                    applyTweakIfNecessary(new CustomScriptTweak(httpClient), new BaseTweakParams(customScriptAbsolutePath)),
-                    applyTweakIfNecessary(new VisualElementsManifestTweak(), new VisualElementsManifestTweakParams(visualElementsSourcePath, visualElementsDestinationPath)));
-            } catch (AggregateException e) {
-                if (e.InnerExceptions.Where(exception => exception is TweakException).Cast<TweakException>().FirstOrDefault() is { } tweakException) {
-                    throw tweakException;
-                } else {
-                    throw e.InnerException!;
-                }
+        foreach ((RegistryKey hive, string path) in uninstallKeys) {
+            using RegistryKey? key = hive.OpenSubKey(path, false);
+            if (key != null) {
+                return (string) key.GetValue("InstallLocation");
             }
         }
 
-        /// <exception cref="TweakException"></exception>
-        private static async Task applyTweakIfNecessary<OutputType, Params>(Tweak<OutputType, Params> tweak, Params tweakParams) where Params: TweakParams where OutputType: class {
-            OutputType? editedFile = await tweak.readFileAndEditIfNecessary(tweakParams);
-            if (editedFile != null) {
-                await tweak.saveFile(editedFile, tweakParams);
-            }
-        }
+        throw new InvalidOperationException("Could not find Vivaldi uninstallation key in registry");
+    }
 
-        /// <exception cref="InvalidOperationException"></exception>
-        private static string getVivaldiApplicationDirectory() {
-            if (Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\vivaldi.exe", "Path", null) is string appPath) {
-                return appPath;
-            }
+    private static string getResourceDirectory(string applicationDirectory) {
+        string versionDirectory = Directory.EnumerateDirectories(applicationDirectory)
+            .Where(absoluteSubdirectory => {
+                string relativeSubdirectory = Path.GetFileName(absoluteSubdirectory)!;
+                return Regex.IsMatch(relativeSubdirectory, @"\A(?:\d+\.){3}\d+\z");
+            })
+            .OrderByDescending(Path.GetFileName, VERSION_NUMBER_COMPARER)
+            .First();
 
-            const string UNINSTALL_REGISTRY_PATH       = @"Software\Microsoft\Windows\CurrentVersion\Uninstall\Vivaldi";
-            const string UNINSTALL_REGISTRY_PATH_WOW64 = @"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Vivaldi";
-            IEnumerable<(RegistryKey hive, string path)> uninstallKeys = new[] {
-                (Registry.CurrentUser, UNINSTALL_REGISTRY_PATH),
-                (Registry.CurrentUser, UNINSTALL_REGISTRY_PATH_WOW64),
-                (Registry.LocalMachine, UNINSTALL_REGISTRY_PATH),
-                (Registry.LocalMachine, UNINSTALL_REGISTRY_PATH_WOW64)
-            };
+        return Path.Combine(versionDirectory, "resources", "vivaldi");
+    }
 
-            foreach ((RegistryKey hive, string path) in uninstallKeys) {
-                using RegistryKey? key = hive.OpenSubKey(path, false);
-                if (key != null) {
-                    return (string) key.GetValue("InstallLocation");
-                }
-            }
+    private static IEnumerable<string> customizeArguments(IEnumerable<string> originalArguments) {
+        IList<string> customizedArguments = originalArguments.ToList();
+        // Turning on web accessibility makes Vivaldi 3 very slow, so use my WebAutoType fork that gets current URL posted from custom.js using Ajax.
+        // customizedArguments.Insert(0, "--force-renderer-accessibility");
+        return customizedArguments;
+    }
 
-            throw new InvalidOperationException("Could not find Vivaldi uninstallation key in registry");
-        }
-
-        private static string getResourceDirectory(string applicationDirectory) {
-            string versionDirectory = Directory.EnumerateDirectories(applicationDirectory)
-                .Where(absoluteSubdirectory => {
-                    string relativeSubdirectory = Path.GetFileName(absoluteSubdirectory)!;
-                    return Regex.IsMatch(relativeSubdirectory, @"\A(?:\d+\.){3}\d+\z");
-                })
-                .OrderByDescending(Path.GetFileName, new VersionNumberComparer())
-                .First();
-
-            return Path.Combine(versionDirectory, "resources", "vivaldi");
-        }
-
-        private static IEnumerable<string> customizeArguments(IEnumerable<string> originalArguments) {
-            IList<string> customizedArguments = originalArguments.ToList();
-            // Turning on web accessibility makes Vivaldi 3 very slow, so use my WebAutoType fork that gets current URL posted from custom.js using Ajax.
-            // customizedArguments.Insert(0, "--force-renderer-accessibility");
-            return customizedArguments;
-        }
-
-        private static int createProcess(string process, string arguments) {
-            return Process.Start(process, arguments)?.Id ?? -1;
-        }
-
+    private static int createProcess(string process, string arguments) {
+        return Process.Start(process, arguments)?.Id ?? -1;
     }
 
 }
