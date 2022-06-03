1f9371ba4cbae8d1fb1457fd06cfc86c2149f87b
Ben Hutchison <ben@aldaviva.com>
2022-06-02 18:08:19 -0700

    Fixed incorrect URL of Javascript file for custom feed page

--- VivaldiCustomLauncher/Tweaks/CustomFeedScriptTweak.cs
+++ VivaldiCustomLauncher/Tweaks/CustomFeedScriptTweak.cs
@@ -7,7 +7,7 @@ namespace VivaldiCustomLauncher.Tweaks;
 
 public class CustomFeedScriptTweak: BaseDownloadableTweak {
 
-    protected override Uri downloadUri { get; } = new("https://github.com/Aldaviva/VivaldiCustomResources/raw/master/scripts/showfeed-custom.js");
+    protected override Uri downloadUri { get; } = new("https://github.com/Aldaviva/VivaldiCustomResources/raw/master/scripts/custom-feed.js");
 
     public CustomFeedScriptTweak(HttpClient httpClient): base(httpClient) { }
 
