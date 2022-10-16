Aldaviva/VivaldiCustomLauncher

commit 9d2fc098c625767908ea868634f0eef51d19ec89

Speed up the tweak to increase the maximum tab width by avoiding variable-length initial wildcard backtracking in pattern, reducing tweak's smoke test duration from 0.44 seconds to 0.07 seconds.

VivaldiCustomLauncher/Tweaks/BundleScriptTweak.cs
@@ -95,8 +95,8 @@ public class BundleScriptTweak: AbstractScriptTweak {
 
     /// <exception cref="TweakException">if the tweak can't be applied</exception>
     internal virtual string increaseMaximumTabWidth(string bundleContents) => replaceOrThrow(bundleContents,
-        new Regex(@"(?<prefix>\b[\w$]{1,3}=)180(?=[;,])"),
-        match => match.Groups["prefix"].Value + 4000 + CUSTOMIZED_COMMENT,
+        new Regex(@"=180(?=[;,])"),
+        _ => '=' + 4000 + CUSTOMIZED_COMMENT,
         new TweakException("Failed to find old max tab width to replace", TWEAK_TYPE));
 
     /// <exception cref="TweakException">if the tweak can't be applied</exception>

commit 84a0e663a3444c3acf92f4a995075caf9bd80d0b

#6: Vivaldi 5.5.2805.38 never loads with 1.2.4 and resources b59f91583acbbc4030866a3626d2d9f7f1fb8b54 [9d2fc09 introduced type confusion regression in 1.2.4, inferring int32 addition instead of string concatenation]

VivaldiCustomLauncher/Tweaks/BundleScriptTweak.cs
@@ -96,7 +96,7 @@ public class BundleScriptTweak: AbstractScriptTweak {
     /// <exception cref="TweakException">if the tweak can't be applied</exception>
     internal virtual string increaseMaximumTabWidth(string bundleContents) => replaceOrThrow(bundleContents,
         new Regex(@"=180(?=[;,])"),
-        _ => '=' + 4000 + CUSTOMIZED_COMMENT,
+        _ => "=4000" + CUSTOMIZED_COMMENT,
         new TweakException("Failed to find old max tab width to replace", TWEAK_TYPE));
 
     /// <exception cref="TweakException">if the tweak can't be applied</exception>
@@ -162,7 +162,7 @@ public class BundleScriptTweak: AbstractScriptTweak {
 
         Match folderManagerVarMatch = new Regex(@"\b(?<folderManagerVar>[\w$.]{1,7}?)\.getPathsByType\(").Match(bundleContents, searchStart);
         if (!folderManagerVarMatch.Success) {
-            throw new TweakException("Failed to find folder manager variable (on which to call getFolders())", TWEAK_TYPE, nameof(allowMovingMailBetweenAnyFolders));
+            throw new TweakException("Failed to find folder manager variable (on which to call getFolders())", TWEAK_TYPE);
         }
 
         string folderManagerVar = folderManagerVarMatch.Groups["folderManagerVar"].Value;